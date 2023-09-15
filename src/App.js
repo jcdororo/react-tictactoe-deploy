4196d385c4185c142368a9fa8ebc21a65b5bf822
jc <wnwlcks123@gmail.com>
2023-09-15 13:25:29 +0900

    Board, Square 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Board from "./components/Board";
+
 
 function App() {
   return (
     <div className="game">
       <div className="game-board">
-
+      <Board />
       </div>
       <div className="game-info">
       </div>
---
+++ src/components/Board.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Square } from './Square'
+import "./Board.css"
+
+export default class Board extends Component {
+
+  renderSqueare(i) {
+    return <Square />
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='status'>Next Player: X ,0</div>
+        <div className='board-row'>
+          {this.renderSqueare(0)}
+          {this.renderSqueare(1)}
+          {this.renderSqueare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSqueare(3)}
+          {this.renderSqueare(4)}
+          {this.renderSqueare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSqueare(6)}
+          {this.renderSqueare(7)}
+          {this.renderSqueare(8)}
+        </div>
+      </div>
+    )
+  }
+}
