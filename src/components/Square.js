de5368744df13dbb17c7cd7a053f0969b39df873
jc <wnwlcks123@gmail.com>
2023-09-15 15:57:04 +0900

    승자 결정하기

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,13 @@
-import React from "react"
-import "./Square.css"
+import React from "react";
+import "./Square.css";
 
-
-  
 const Square = ({ onClick, value }) => {
-  return (
-    <button className="square" 
-      onClick={onClick}>
-      {value}
-    </button>
-  )
+    return (
+        <button className="square"
+            onClick={onClick}>
+            {value}
+        </button>
+    )
 }
 
 export default Square;
-  
-  
