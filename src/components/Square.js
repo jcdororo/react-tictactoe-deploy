0ac054536b36611c028492251761ae8167cd029f
jc <wnwlcks123@gmail.com>
2023-09-15 14:12:48 +0900

    부모 컴포넌트에서 State 보관하기

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,18 +2,13 @@ import React from "react"
 import "./Square.css"
 
 export class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
+  
 
 
   render() {
     return (
       <button className="square" onClick={() => {this.setState({value: 'X'})}}>
-        {this.state.value}
+        {this.props.value}
       </button>
     )
   }
