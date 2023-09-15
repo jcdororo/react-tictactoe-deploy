dc6f713c04cee504ca40cf535fac1ef4044ee23f
jc <wnwlcks123@gmail.com>
2023-09-15 16:37:38 +0900

    동작에 대한 기록 저장하기

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,83 +1,33 @@
 import React, { useState } from 'react'
 import Square from './Square'
-import "./Board.css"
+import "./Board.css";
 
-const Board = () =>{
+const Board = ({squares, onClick}) => {
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xisNext, setXIsNext] = useState(true);
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    for (let i = 0; i < lines.length; i++) {
-      const [a,b,c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if(winner){
-    status = `Winner: ${winner}`;
-  } else {
-    status = `Next player: ${xisNext? 'X' : 'O'}`;
-  }
-
-  
-  const handleCLick = (i) => {
-    const newSquares = squares.slice();
-
-    if(calculateWinner(newSquares) || newSquares[i]) {
-      return;
+    const renderSquare = (i) => {
+        return <Square value={squares[i]}
+            onClick={() => onClick(i)} />
     }
 
-    newSquares[i] = xisNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXIsNext(!xisNext);
-  }
-
-  const renderSqueare = (i) => {
-    return <Square value={squares[i]} 
-      onClick={() => handleCLick(i)} />
-  }
-
-
-
-  
-  return (
-    <div>
-      <div className='status'>{status}</div>
-      <div className='board-row'>
-        {renderSqueare(0)}
-        {renderSqueare(1)}
-        {renderSqueare(2)}
-      </div>
-      <div className='board-row'>
-        {renderSqueare(3)}
-        {renderSqueare(4)}
-        {renderSqueare(5)}
-      </div>
-      <div className='board-row'>
-        {renderSqueare(6)}
-        {renderSqueare(7)}
-        {renderSqueare(8)}
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className='board-row'>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    )
 }
 
-
 export default Board
-
