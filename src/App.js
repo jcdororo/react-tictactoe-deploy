04aaafa10b8d60b636d82bc1b435d0aef02d0443
jc <wnwlcks123@gmail.com>
2023-09-16 13:32:11 +0900

    TicTacToe 앱 스타일링 변경하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-// import "/App.css";
+import "./App.css"
 import Board from "./components/Board";
-
 
 function App() {
 
@@ -11,17 +10,17 @@ function App() {
 
   const calculateWinner = (squares) => {
     const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
     ]
-    for (let i = 0; i < lines.length; i++) {
-      const [a,b,c] = lines[i];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
@@ -33,10 +32,10 @@ function App() {
   const winner = calculateWinner(current.squares);
 
   let status;
-  if(winner){
-    status = `Winner: ${winner}`;
+  if (winner) {
+    status = 'Winner: ' + winner;
   } else {
-    status = `Next player: ${xIsNext? 'X' : 'O'}`;
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
   }
 
   const handleClick = (i) => {
@@ -73,14 +72,14 @@ function App() {
   return (
     <div className="game">
       <div className="game-board">
-      <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
-      <div className="game-info">        
+      <div className="game-info">
         <div className='status'>{status}</div>
-        <ol>{moves}</ol>
+        <ol style={{ listStyle: 'none' }}>{moves}</ol>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
